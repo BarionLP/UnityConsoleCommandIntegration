1d05a0c84b9005c5e6239385410d787580d1287b
Barion <kothe@botos.de>
2023-07-02 16:44:30 +0200

    init

---
+++ Runtime/ConsoleCommandHandler.cs
@@ -0,0 +1,18 @@
+using System;
+using Ametrin.Console;
+using Ametrin.Command;
+
+namespace Ametrin.ConsoleCommandIntegration{
+    #nullable enable
+    public sealed class ConsoleCommandHandler : IConsoleHandler {
+        public bool PassPrefix => false;
+
+        public void Execute(string input)=> CommandManager.Execute(input);
+
+        public string? GetSyntax(string value){
+            var inputParts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if(inputParts.Length == 0) return null;
+            return CommandManager.GetSyntax(inputParts[0]);
+        }
+    }
+}
