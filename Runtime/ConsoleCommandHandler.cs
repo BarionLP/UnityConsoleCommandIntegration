ab0fee7bfb1ca9417e410d6160de8ef3cd584318
Barion <ametrin.studios@gmail.com>
2023-08-27 17:33:30 +0200

    span port

--- Runtime/ConsoleCommandHandler.cs
+++ Runtime/ConsoleCommandHandler.cs
@@ -1,24 +1,25 @@
 using System;
 using Ametrin.Console;
 using Ametrin.Command;
+using Ametrin.Utils;
 
 namespace Ametrin.ConsoleCommandIntegration{
     #nullable enable
     public sealed class ConsoleCommandHandler : IConsoleHandler {
         public bool PassPrefix => false;
 
-        public void Execute(string input)=> CommandManager.Execute(input);
+        public void Execute(ReadOnlySpan<char> input)=> CommandManager.Execute(input);
 
-        public string? GetSyntax(string value){
-            var inputParts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            if(inputParts.Length == 0) return CommandManager.GetFirstSyntax();
-            return CommandManager.GetSyntax(inputParts[0]);
+        public ReadOnlySpan<char> GetSyntax(ReadOnlySpan<char> input){
+            var inputParts = input.Split(' ');
+            if(inputParts.Count == 0) return CommandManager.GetFirstSyntax();
+            return CommandManager.GetSyntax(input[inputParts[0]]);
         }
 
-        public string? GetCompletion(string input){
-            var inputParts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            if (inputParts.Length == 0) return null;
-            return CommandManager.GetFirstCommand(inputParts[0])?.Remove(0, input.Length) + " ";
+        public ReadOnlySpan<char> GetAutoCompleted(ReadOnlySpan<char> input){
+            var inputParts = input.Split(' ');
+            if (inputParts.Count != 1) return ReadOnlySpan<char>.Empty;
+            return CommandManager.GetFirstCommand(input[inputParts[0]]) + " ";
         }
     }
 }
