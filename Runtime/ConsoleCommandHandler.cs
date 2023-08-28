652a597e5ff81d13aaab422c376ac69d46314c9a
Barion <ametrin.studios@gmail.com>
2023-08-28 14:44:54 +0200

    implemented parameter completion

--- Runtime/ConsoleCommandHandler.cs
+++ Runtime/ConsoleCommandHandler.cs
@@ -2,9 +2,10 @@ using System;
 using Ametrin.Console;
 using Ametrin.Command;
 using Ametrin.Utils;
+using System.Text;
 
 namespace Ametrin.ConsoleCommandIntegration{
-    #nullable enable
+#nullable enable
     public sealed class ConsoleCommandHandler : IConsoleHandler {
         public bool PassPrefix => false;
 
@@ -16,10 +17,18 @@ namespace Ametrin.ConsoleCommandIntegration{
             return CommandManager.GetSyntax(input[inputParts[0]]);
         }
 
-        public ReadOnlySpan<char> GetAutoCompleted(ReadOnlySpan<char> input){
+        public string GetAutoCompleted(ReadOnlySpan<char> input){
             var inputParts = input.Split(' ');
-            if (inputParts.Count != 1) return ReadOnlySpan<char>.Empty;
-            return CommandManager.GetFirstCommand(input[inputParts[0]]) + " ";
+            if(inputParts.Count == 0) return CommandManager.GetFirstCommand();
+            var key = input[inputParts[0]];
+            var blank = input[^1] == ' ';
+            if(inputParts.Count == 1 && !blank) return CommandManager.GetFirstCommand(key);
+
+            if (CommandManager.GetCommand(key).HasFailed(out var command)) return string.Empty;
+            inputParts.RemoveAt(0);
+            var nextParam = command.CompleteNextParameter(input, inputParts, blank);
+            var start = blank ? input : input[..inputParts[^1].Start];
+            return new StringBuilder(start.Length + nextParam.Length).Append(start).Append(nextParam).ToString();
         }
     }
 }
